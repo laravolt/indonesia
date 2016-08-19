da3b70d716cc29fb916852f06fd6d851ed81a541
deriramdanis <deri.ramdani1@gmail.com>
2016-08-19 14:30:56 +0700

    add custom attribute

--- src/models/Province.php
+++ src/models/Province.php
@@ -10,4 +10,17 @@ class Province extends Model
     {
         return $this->hasMany('Laravolt\Indonesia\Models\Regency', 'province_id');
     }
+	
+	public function getLogoPathAttribute()
+    {
+    	$folder = 'indonesia-logo/';
+    	$id = $this->getAttributeValue('id');
+     	$arr_glob = glob(public_path().'/'. $folder. $id.'.*');
+     	if(count($arr_glob) == 1){
+     		$logo_name = basename($arr_glob[0]);
+     		$logo_path = url($folder.$logo_name);
+     		return $logo_path;
+     	}
+     	return null;
+    }
 }
--- src/models/Regency.php
+++ src/models/Regency.php
@@ -15,4 +15,17 @@ class Regency extends Model
     {
         return $this->hasMany('Laravolt\Indonesia\Models\District', 'regency_id');
     }
+	
+	public function getLogoPathAttribute()
+    {
+    	$folder = 'indonesia-logo/';
+    	$id = $this->getAttributeValue('id');
+     	$arr_glob = glob(public_path().'/'. $folder. $id.'.*');
+     	if(count($arr_glob) == 1){
+     		$logo_name = basename($arr_glob[0]);
+     		$logo_path = url($folder.$logo_name);
+     		return $logo_path;
+     	}
+     	return null;
+    }
 }
