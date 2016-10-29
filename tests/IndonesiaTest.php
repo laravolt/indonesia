573be325d94994612f329d7914f6bac26ec67a78
Adhatama <am.adhatama@gmail.com>
2016-10-29 23:56:54 +0700

    enhance seeder test case

--- tests/IndonesiaTest.php
+++ tests/IndonesiaTest.php
@@ -10,5 +10,14 @@ class IndonesiaTest extends TestCase
     {
         $this->seeInDatabase('provinces', ['name' => 'ACEH']);
         $this->seeInDatabase('provinces', ['name' => 'PAPUA']);
+
+        $this->seeInDatabase('cities', ['name' => 'KABUPATEN SIMEULUE']);
+        $this->seeInDatabase('cities', ['name' => 'KOTA JAYAPURA']);
+
+        $this->seeInDatabase('districts', ['name' => 'TEUPAH SELATAN']);
+        $this->seeInDatabase('districts', ['name' => 'RUMBIO JAYA']);
+
+        $this->seeInDatabase('villages', ['name' => 'LATIUNG']);
+        $this->seeInDatabase('villages', ['name' => 'PAYA KALUT']);
     }
 }
