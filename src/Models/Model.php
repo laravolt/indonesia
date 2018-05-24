e4717a576b61b19d00210365deaed288f93b54d7
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2018-05-24 15:48:28 +0700

    Fix mismatch value for ID caused by max int size in PHP

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -4,6 +4,8 @@ namespace Laravolt\Indonesia\Models;
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
+    protected $keyType = 'string';
+
     function __construct()
     {
     	$this->table = config('laravolt.indonesia.table_prefix') . $this->table;
