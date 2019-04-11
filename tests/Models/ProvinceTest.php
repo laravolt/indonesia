a579e9cc8aa82475386aa4e235a4c8e501a99731
Nafies Luthfi <nafiesl@gmail.com>
2019-04-11 18:26:32 +0800

    Add ProvinceTest

---
+++ tests/Models/ProvinceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Laravolt\Indonesia\Test\Models;
+
+use Laravolt\Indonesia\Models\City;
+use Laravolt\Indonesia\Test\TestCase;
+use Laravolt\Indonesia\Models\District;
+use Laravolt\Indonesia\Models\Province;
+use Illuminate\Database\Eloquent\Collection;
+
+class ProvinceTest extends TestCase
+{
+    /** @test */
+    public function a_province_has_many_cities_relation()
+    {
+        $this->seed('Laravolt\Indonesia\Seeds\ProvincesSeeder');
+        $this->seed('Laravolt\Indonesia\Seeds\CitiesSeeder');
+        $province = Province::first();
+
+        $this->assertInstanceOf(Collection::class, $province->cities);
+        $this->assertInstanceOf(City::class, $province->cities->first());
+    }
+
+    /** @test */
+    public function a_province_has_many_districts_relation()
+    {
+        $this->seed('Laravolt\Indonesia\Seeds\ProvincesSeeder');
+        $this->seed('Laravolt\Indonesia\Seeds\CitiesSeeder');
+        $this->seed('Laravolt\Indonesia\Seeds\DistrictsSeeder');
+        $province = Province::first();
+
+        $this->assertInstanceOf(Collection::class, $province->districts);
+        $this->assertInstanceOf(District::class, $province->districts->first());
+    }
+}
