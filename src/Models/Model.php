5b01eb06804241024a5887728dee050ccae05ec0
Nabil Muh. Firdaus <nabilftd@gmail.com>
2019-06-11 09:02:05 +0700

    hotfix: Issue #35 - Tambah atribut di konstruktor dan panggil parent (#36)

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -6,8 +6,10 @@ class Model extends \Illuminate\Database\Eloquent\Model
 {
     protected $keyType = 'string';
 
-    function __construct()
+    public function __construct(array $attributes = [])
     {
+        parent::__construct($attributes);
+
     	$this->table = config('laravolt.indonesia.table_prefix') . $this->table;
     }
 
