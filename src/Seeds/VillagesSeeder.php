cee9488026e0e02f00b930f89172db66fd0cf4c7
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-08-23 15:52:29 +0700

    Chunk villages seeder to avoid memory leak

--- src/Seeds/VillagesSeeder.php
+++ src/Seeds/VillagesSeeder.php
@@ -8,18 +8,30 @@ class VillagesSeeder extends Seeder
 {
     public function run()
     {
-    	$Csv = new CsvtoArray;
-        $file = __DIR__. '/../../resources/csv/villages.csv';
-        $header = array('id', 'district_id', 'name');
-        $data = $Csv->csv_to_array($file, $header);
-        $data = array_map(function ($arr) {
-            return $arr + ['created_at' => now()];
-        }, $data);
+        $file = __DIR__.'/../../resources/csv/villages.csv';
+        $delimiter = ',';
 
-        $collection = collect($data);
-        foreach($collection->chunk(50) as $chunk) {
-            \DB::table(config('laravolt.indonesia.table_prefix') . 'villages')->insert($chunk->toArray());
+        if (!file_exists($file) || !is_readable($file)) {
+            return false;
+        }
+
+        $chunk = [];
+        $header = ['id', 'district_id', 'name'];
+        if (($handle = fopen($file, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
+                $chunk[] = array_combine($header, $row);
+
+                if (count($chunk) === 100) {
+                    \DB::table(config('laravolt.indonesia.table_prefix').'villages')->insert($chunk);
+                    $chunk = [];
+                }
+            }
+
+            if (count($chunk) > 0) {
+                \DB::table(config('laravolt.indonesia.table_prefix').'villages')->insert($chunk);
+            }
+
+            fclose($handle);
         }
     }
-
 }
