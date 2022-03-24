4e9c888592ae0c9cde30585b0cf0f17f5c4e8442
rama <rama@javan.co.id>
2022-03-24 13:33:58 +0700

    refactor: use address trait

---
+++ src/Models/Traits/AddressTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Laravolt\Indonesia\Models\Traits;
+
+use Laravolt\Indonesia\Models\Province;
+
+/**
+ * getAddressAttribute
+ */
+trait AddressTrait
+{
+    public function getAddressAttribute()
+    {
+        switch (get_class($this)) {
+            case 'Laravolt\Indonesia\Models\Kabupaten':
+                $this->load('province');
+
+                $address = sprintf('%s, %s', $this->name, $this->province_name);
+                break;
+
+            case 'Laravolt\Indonesia\Models\Kecamatan':
+                $this->load('city.province');
+
+                $address = sprintf('%s, %s, %s', $this->name, $this->city_name, $this->province_name);
+                break;
+
+            case 'Laravolt\Indonesia\Models\Kelurahan':
+                $this->load('district.city.province');
+
+                $data = [$this->name, $this->district_name, $this->city_name, $this->province_name];
+                $address = vsprintf('%s, %s, %s, %s', $data);
+                break;
+
+            default:
+                $address = $this->name;
+                break;
+        }
+
+        return "$address, Indonesia";
+    }
+}
