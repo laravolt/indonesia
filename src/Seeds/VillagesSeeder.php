26f98ee28be8a2f5fd63c8cd277589964854d6b5
rama <rama@javan.co.id>
2022-03-25 19:44:19 +0700

    feature: meta pos di desa

--- src/Seeds/VillagesSeeder.php
+++ src/Seeds/VillagesSeeder.php
@@ -15,12 +15,12 @@ class VillagesSeeder extends Seeder
         $csv = new CsvtoArray();
         $resourceFiles = File::allFiles(__DIR__.'/../../resources/csv/villages');
         foreach ($resourceFiles as $file) {
-            $header = ['code', 'district_code', 'name', 'lat', 'long'];
+            $header = ['code', 'district_code', 'name', 'lat', 'long', 'pos'];
             $data = $csv->csv_to_array($file->getRealPath(), $header);
 
             $data = array_map(function ($arr) use ($now) {
-                $arr['meta'] = json_encode(['lat' => $arr['lat'], 'long' => $arr['long']]);
-                unset($arr['lat'], $arr['long']);
+                $arr['meta'] = json_encode(['lat' => $arr['lat'], 'long' => $arr['long'], 'pos' => $arr['pos']]);
+                unset($arr['lat'], $arr['long'], $arr['pos']);
 
                 return $arr + ['created_at' => $now, 'updated_at' => $now];
             }, $data);
